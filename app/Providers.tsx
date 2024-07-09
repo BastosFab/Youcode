db1e0c60ae9ff0ce52254833378f102b7d7d80c0
BastosFab <fabienvdbg@gmail.com>
2024-07-09 10:12:46 +0200

    T1 - Setup Application

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -5,13 +5,23 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
 import { Toaster } from "sonner";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Toaster expand={true} richColors />
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <QueryClientProvider client={queryClient}>
+            {/* The rest of your application */}
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </SessionProvider>
       </ThemeProvider>
     </>
   );
